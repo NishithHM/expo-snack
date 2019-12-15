a977b2508084d582a8e1f476919831a81aee89fc
Nishith <nhm987@gmail.com>
2019-12-15 18:35:16 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey I am being imported from git to snackbar and rendering in all phones connected to my server, this would be great idea for client demos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
